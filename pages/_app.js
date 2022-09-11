6a773cd42fb293ec55c85c13e517b5cdad9d11ae
Chris Jaure <chrisjaure@gmail.com>
2022-09-11 16:34:07 -0500

    add some styles and navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import "@code-hike/mdx/styles"
+import "../styles/global.css"
+import Link from "next/link"
 
 function MyApp({ Component, pageProps }) {
+  const components = {
+    ...pageProps.components,
+    a: Link
+  }
+
   return (
-    <article style={{ maxWidth: 768, margin: "0 auto" }}>
-      <Component {...pageProps} />
-    </article>
+    <div className="min-h-screen p-20 bg-slate-300">
+      <article className="container prose prose-slate lg:prose-lg mx-auto shadow-md p-8 bg-slate-50">
+        <Component {...pageProps} components={components} />
+      </article>
+    </div>
   )
 }
 
 export default MyApp
+
