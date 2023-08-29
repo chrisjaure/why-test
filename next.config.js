551bb01b93220ef808866f0bd82bfde588e39c81
Chris Jaure <chrisjaure@gmail.com>
2023-08-29 08:46:48 -0500

    major dep upgrades

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const { remarkCodeHike } = require("@code-hike/mdx")
-const theme = require("shiki/themes/material-default.json")
 
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [[remarkCodeHike, { theme }]],
+    remarkPlugins: [[remarkCodeHike, { theme: 'material-default' }]],
   },
 })
 
